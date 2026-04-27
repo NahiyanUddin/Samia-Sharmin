97c14d8c5788af4b704782679dd715011d473282
Md Nahiyan Uddin <nahiyan.uddin.102@gmail.com>
2026-04-26 22:26:27 -0500

    Updated Homepage

--- assets/js/site.js
+++ assets/js/site.js
@@ -215,8 +215,10 @@ function renderHome(main, root, content, writtenArticles, mentions, introParagra
     <section class="section section-spaced split-band">
       <div class="quote-panel">
         <div class="eyebrow">Practice Statement</div>
-        <p>${escapeHtml(introParagraphs[0] || profile.homepage_intro.short)}</p>
-        <p>${escapeHtml(introParagraphs[1] || profile.homepage_intro.long)}</p>
+        <blockquote>${escapeHtml(introParagraphs[0] || profile.homepage_intro.short)}</blockquote>
+        <br>
+        <br>
+        <blockquote>${escapeHtml(introParagraphs[1] || profile.homepage_intro.long)}</blockquote>
       </div>
       <div class="section-panel rich-copy">
         <div class="section-heading">
